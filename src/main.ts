e28e8e1d4099216b9c65d42a572e3dcd681d0e8a
Florian Van Belleghem <fvanbelleghem@icloud.com>
2025-03-03 15:26:05 +0100

    chore: initial scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,53 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { WebGPURenderer } from "three/webgpu";
+import * as THREE from "three";
+import * as TSL from "three/tsl";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const renderer = new WebGPURenderer({
+  canvas,
+  antialias: true,
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const scene = createScene();
+const camera = createCamera();
+scene.add(camera);
+start();
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+function start() {
+  window.addEventListener("resize", () => {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+  window.dispatchEvent(new Event("resize"));
+  render();
+}
+
+function createScene() {
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  return scene;
+}
+
+function createCamera() {
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    canvas.width / canvas.height,
+    0.1,
+    100
+  );
+  camera.position.z = 5;
+
+  return camera;
+}
